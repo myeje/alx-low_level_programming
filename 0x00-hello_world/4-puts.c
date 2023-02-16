195cb6a21174fcce1d769c12f8ebac24c14118b8
Joshua Emore <ehimareemore@gmail.com>
2023-02-16 13:19:53 -0800

    Write C program with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char MyStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(MyStr);
 
-	return 0;
+	return (0);
 }
