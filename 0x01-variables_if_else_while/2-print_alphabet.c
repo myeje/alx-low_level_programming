835e8c9dd0f5b9cf47f02af4d35365df23924e7b
Joshua Emore <ehimareemore@gmail.com>
2023-02-17 09:06:36 -0800

    Printing alphabets in lowercase with for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = a; a <= z; alph++)
+		putchar(alph);
+		putchar(\n);
+
+	return (0);
+}
