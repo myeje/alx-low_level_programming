53b319476a0bc2199b124b8eaa4bee177d936e1c
Joshua Emore <ehimareemore@gmail.com>
2023-02-16 17:04:50 -0800

    Writes c program that outputs to the standard error(???)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - Entry point
  * Return: Always 1 (Success)
@@ -7,6 +8,7 @@
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 20);
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
