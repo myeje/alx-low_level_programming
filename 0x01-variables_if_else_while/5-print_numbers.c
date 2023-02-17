8744cce50a1326f60a720c0765dca51ad3d8fe23
Joshua Emore <ehimareemore@gmail.com>
2023-02-17 09:55:38 -0800

    Prints all single digits of base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	int num;
+        int num;
 
-	for (num = 0; num < 10; num++)
-		printf(num);
-	printf(\n);
-
-	return (0);
+        for (num = 0; num < 10; num++)
+                printf("%d", num);
+        printf("\n");
+        return (0);
 }
+
