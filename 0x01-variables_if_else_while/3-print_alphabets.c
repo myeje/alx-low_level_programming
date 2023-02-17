81c7ad9287b0927e68fd24778d8d3fde9b9c6041
Joshua Emore <ehimareemore@gmail.com>
2023-02-17 09:40:38 -0800

    Printing alphabets in lower and upper case with for loop

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char alp;
 
-	for (alp = 'a'; 'a' <= 'z'; alp++)
+	for (alp = 'a'; alp <= 'z'; alp++)
 		putchar(alp);
-	for (alp = 'A'; 'A' <= 'Z'; alp++)
+	for (alp = 'A'; alp <= 'Z'; alp++)
 		putchar(alp);
 	putchar('\n');
 
