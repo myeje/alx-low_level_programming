c105718da12fee1c1a7ad3f814ba0be1bead91d5
Joshua Emore <ehimareemore@gmail.com>
2023-02-21 06:06:52 -0800

    Prints out _putchar with a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<unistd.h>
+#include "_putchar.h"
+/**
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		putchar(word[a]);
+
+	putchar('\n');
+
+	return (0);
+}
