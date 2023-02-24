9adae4f9ffe222597b79926087ec561af7081be6
Joshua Emore <ehimareemore@gmail.com>
2023-02-24 13:15:53 -0800

    Prints 10 times the number from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,14 @@ void more_numbers(void)
 {
 	int a, b = 0;
 
-	for (; a < 10; a++)
+	for (; b < 10; b++)
 	{
-		for (b = 0; b <= 14; b++)
+		for (a = 0; a <= 14; a++)
 		{
-			if (b > 9)
-				_putchar(b / 10 + '0');
+			if (a > 9)
+				_putchar(a / 10 + '0');
 
-			_putchar(b % 10 + '0');
+			_putchar(a % 10 + '0');
 		}
 		_putchar('\n');
 	}
