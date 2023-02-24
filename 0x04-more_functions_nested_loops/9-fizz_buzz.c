19c5a1bc1452a8e3f0c046aeb37cb96e8e5fe4b4
Joshua Emore <ehimareemore@gmail.com>
2023-02-24 07:18:09 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,22 +7,14 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (a % 3 == 0)
-		{
 			printf("Fizz ");
-		}
 		else if (a % 5 == 0)
-		{
 			printf("Buzz ");
-		}
 		else
-		{
 			printf("%d ", a);
-		}
 	}
-	printf("Buzz\n")
+
 	return (0);
 }
