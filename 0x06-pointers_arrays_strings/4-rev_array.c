f1020a2a4214e01c412b3c94610d27ee16150ff2
Joshua Emore <ehimareemore@gmail.com>
2023-03-02 07:31:00 -0800

    Reverses the content of an array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j;
-	char temp;
+	int i = 0;
+	int temp;
 
-	for (i = 0, j = n - 1; i < j; i++, j--)
+	while (i < n--)
 	{
 		temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
+		a[i++] = a[n];
+		a[n] = temp;
 	}
 }
