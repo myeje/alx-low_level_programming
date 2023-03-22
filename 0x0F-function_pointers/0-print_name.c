e31ab65324b96858e0002a0db70a19c125ce22b5
Joshua Emore <ehimareemore@gmail.com>
2023-03-22 10:31:39 -0700

    Funtion that prints a name using the pointer function passed as an argument

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - takes in a function argument that prints its character argument
+ * @name: name to be printed
+ * @f: pointer function argument that prints name
+ * Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	f(ne);
+	f(name);
 }
