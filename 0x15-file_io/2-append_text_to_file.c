6f77c75c7134aedda3ef5a6e8fd5a587a65e07f5
Joshua Emore <ehimareemore@gmail.com>
2023-04-29 02:05:36 -0700

    Function that appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,13 +24,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content)
-	{
-		length = strlen(text_content);
-		write_file = write(open_file, text_content, length);
-		if (write_file < 0)
-			return (-1);
-	}
+	length = strlen(text_content);
+	write_file = write(open_file, text_content, length);
+	if (write_file < 0)
+		return (-1);
+	
 	close(open_file);
 	return (1);
 }
