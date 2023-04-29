75258a899556ec3ee4dedb937dbbdfc524a4db53
Joshua Emore <ehimareemore@gmail.com>
2023-04-29 01:46:51 -0700

    Function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (; text_content[length]; letter++)
+	length = strlen(text_content);
 
 	write_file = write(open_file, text_content, length);
 	if (write_file < 0)
