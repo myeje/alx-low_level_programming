4450d41fa26b430016d555d9128814a812ccd345
Joshua Emore <ehimareemore@gmail.com>
2023-10-26 02:12:02 +0100

    C

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function searches for a value in array
+ * of integers using linear search algorithm
+ * @array: pointer to first element of array
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: all
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array
+ * using the binary search algorithm
+ * @array: pointer to first element of array
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: all
+ * 
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	int x = 0, y, z = size - 1;
+	int idx;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while (x <= z)
+	{
+		y = (x + z) / 2;
+		printf("Searching in array: ");
+
+		for (idx = x; idx <= z; idx++)
+		{
+			printf("%i%s", array[idx], idx == z ? "\n" : ", ");
+		}
+		if (array[y] < value)
+			x = y + 1;
+		else if (array[y] > value)
+			z = y - 1;
+		else
+			return (y);
+	}
+	return (-1);
+}
